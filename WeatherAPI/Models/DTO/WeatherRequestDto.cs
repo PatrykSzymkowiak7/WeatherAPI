f6371ec5fc7a9dc492639c2b7b9148501db22be2
PatrykSzymkowiak7 <87982924+PatrykSzymkowiak7@users.noreply.github.com>
2025-11-17 18:04:27 +0100

    Created WeatherService and cleaned up WeatherController

---
+++ WeatherAPI/Models/DTO/WeatherRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherAPI.Models.DTO
+{
+    public class WeatherRequestDto
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string City { get; set; } = "";
+    }
+}
